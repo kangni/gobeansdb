a15fde3c847ae161d9aec7d751a0c7e4bd85137d
kangni <2881570+kangni@users.noreply.github.com>
2018-09-11 12:58:09 +0800

    避免依赖安装错误

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -2,6 +2,7 @@
 
 virtualenv venv
 source venv/bin/activate
-venv/bin/python venv/bin/pip install -r tests/pip-req.txt
+venv/bin/python venv/bin/pip install Cython>=0.20 -i https://pypi.douban.com/simple
+venv/bin/python venv/bin/pip install -r tests/pip-req.txt -i https://pypi.douban.com/simple
 venv/bin/python venv/bin/nosetests --with-xunit --xunit-file=unittest.xml
 deactivate
