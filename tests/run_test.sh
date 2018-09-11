ca86787caee9d221cfbf3a2a698ffaf3121bc3b7
kangni <2881570+kangni@users.noreply.github.com>
2018-09-11 13:13:51 +0800

    Update run_test.sh

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -2,7 +2,6 @@
 
 virtualenv venv
 source venv/bin/activate
-venv/bin/python venv/bin/pip install Cython>=0.20 -i https://pypi.douban.com/simple
 venv/bin/python venv/bin/pip install -r tests/pip-req.txt -i https://pypi.douban.com/simple
 venv/bin/python venv/bin/nosetests --with-xunit --xunit-file=unittest.xml
 deactivate
